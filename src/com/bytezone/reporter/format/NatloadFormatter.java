4ebe044a1cded97fad8d9f97e60f97e7b95b117b
Denis Molony <dmolony@iinet.net.au>
2015-07-08 21:57:56 +1000

    trim nulls

--- src/com/bytezone/reporter/format/NatloadFormatter.java
+++ src/com/bytezone/reporter/format/NatloadFormatter.java
@@ -27,18 +27,10 @@ public class NatloadFormatter extends DefaultFormatter
                             program, sequence, lines);
     }
 
-    if (record.length > 150)
-    {
-      int length = record.length - record.countTrailingNulls ();
-      return String.format ("%02X%02X %s", record.buffer[record.offset] & 0xFF,
-                            record.buffer[record.offset + 1] & 0xFF, textMaker
-                                .getText (record.buffer, record.offset + 2, length - 2))
-          .trim ();
-    }
-    return String
-        .format ("%02X%02X %s", record.buffer[record.offset] & 0xFF,
-                 record.buffer[record.offset + 1] & 0xFF,
-                 textMaker.getText (record.buffer, record.offset + 2, record.length - 2))
+    int length = record.length - record.countTrailingNulls ();
+    return String.format ("%02X%02X %s", record.buffer[record.offset] & 0xFF,
+                          record.buffer[record.offset + 1] & 0xFF, textMaker
+                              .getText (record.buffer, record.offset + 2, length - 2))
         .trim ();
   }
 }
