e9c2469995c96c8a362df4326cbbead57b64c893
Denis Molony <dmolony@iinet.net.au>
2015-07-30 19:20:46 +1000

    adapting for reuse

--- src/com/bytezone/reporter/application/ReporterScene.java
+++ src/com/bytezone/reporter/application/ReporterScene.java
@@ -1,25 +1,13 @@
 package com.bytezone.reporter.application;
 
-import java.awt.print.PageFormat;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
 import java.util.prefs.Preferences;
-
-import javax.swing.SwingUtilities;
 
 import com.bytezone.reporter.application.TreePanel.FileNode;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.TreeView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
@@ -35,7 +23,7 @@ public class ReporterScene extends Scene
   private final BorderPane borderPane;
   private final MenuBar menuBar = new MenuBar ();
 
-  private WindowSaver windowSaver;
+  //  private WindowSaver windowSaver;
   private final Preferences prefs = Preferences.userNodeForPackage (this.getClass ());
 
   public ReporterScene (BorderPane root)
@@ -56,10 +44,10 @@ public class ReporterScene extends Scene
     borderPane.setLeft (stackPane);
     borderPane.setTop (menuBar);
 
-    menuBar.getMenus ().addAll (getFileMenu ());
+    //    menuBar.getMenus ().addAll (getFileMenu ());
 
-    if (MAC_MENUBAR)
-      menuBar.useSystemMenuBarProperty ().set (true);
+    //    if (MAC_MENUBAR)
+    //      menuBar.useSystemMenuBarProperty ().set (true);
 
     //    Scene scene = new Scene (borderPane, 800, 592);
     tree.requestFocus ();
@@ -73,84 +61,79 @@ public class ReporterScene extends Scene
     formatBox.setFileNode (fileNode, this);
   }
 
-  private Menu getFileMenu ()
-  {
-    Menu menuFile = new Menu ("File");
+  //  private Menu getFileMenu ()
+  //  {
+  //    Menu menuFile = new Menu ("File");
+  //
+  //    MenuItem menuItemOpen = getMenuItem ("Open...", e -> openFile (), KeyCode.O);
+  //    MenuItem menuItemSave = getMenuItem ("Save...", e -> saveFile (), KeyCode.S);
+  //    MenuItem menuItemPrint = getMenuItem ("Page setup", e -> pageSetup (), null);
+  //    MenuItem menuItemPageSetup = getMenuItem ("Print", e -> printFile (), KeyCode.P);
+  //    MenuItem menuItemClose = getMenuItem ("Close window", e -> closeWindow (), KeyCode.W);
+  //
+  //    menuFile.getItems ().addAll (menuItemOpen, menuItemSave, menuItemPageSetup,
+  //                                 menuItemPrint, menuItemClose);
+  //    return menuFile;
+  //  }
+  //
+  //  private MenuItem getMenuItem (String text, EventHandler<ActionEvent> eventHandler,
+  //      KeyCode keyCode)
+  //  {
+  //    MenuItem menuItem = new MenuItem (text);
+  //    menuItem.setOnAction (eventHandler);
+  //    if (keyCode != null)
+  //      menuItem.setAccelerator (new KeyCodeCombination (keyCode,
+  //          KeyCombination.SHORTCUT_DOWN));
+  //    return menuItem;
+  //  }
 
-    MenuItem menuItemOpen = getMenuItem ("Open...", e -> openFile (), KeyCode.O);
-    MenuItem menuItemSave = getMenuItem ("Save...", e -> saveFile (), KeyCode.S);
-    MenuItem menuItemPrint = getMenuItem ("Page setup", e -> pageSetup (), null);
-    MenuItem menuItemPageSetup = getMenuItem ("Print", e -> printFile (), KeyCode.P);
-    MenuItem menuItemClose = getMenuItem ("Close window", e -> closeWindow (), KeyCode.W);
+  //  private void openFile ()
+  //  {
+  //    System.out.println ("Open");
+  //  }
 
-    menuFile.getItems ().addAll (menuItemOpen, menuItemSave, menuItemPageSetup,
-                                 menuItemPrint, menuItemClose);
-    return menuFile;
-  }
+  //  private void pageSetup ()
+  //  {
+  //    SwingUtilities.invokeLater (new Runnable ()
+  //    {
+  //      @Override
+  //      public void run ()
+  //      {
+  //        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
+  //        PageFormat pageFormat = printerJob.defaultPage ();
+  //        printerJob.pageDialog (pageFormat);
+  //      }
+  //    });
+  //  }
 
-  private MenuItem getMenuItem (String text, EventHandler<ActionEvent> eventHandler,
-      KeyCode keyCode)
-  {
-    MenuItem menuItem = new MenuItem (text);
-    menuItem.setOnAction (eventHandler);
-    if (keyCode != null)
-      menuItem.setAccelerator (new KeyCodeCombination (keyCode,
-          KeyCombination.SHORTCUT_DOWN));
-    return menuItem;
-  }
-
-  private void openFile ()
-  {
-    System.out.println ("Open");
-  }
-
-  private void pageSetup ()
-  {
-    SwingUtilities.invokeLater (new Runnable ()
-    {
-      @Override
-      public void run ()
-      {
-        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
-        PageFormat pageFormat = printerJob.defaultPage ();
-        printerJob.pageDialog (pageFormat);
-      }
-    });
-  }
-
-  private void printFile ()
-  {
-    SwingUtilities.invokeLater (new Runnable ()
-    {
-      @Override
-      public void run ()
-      {
-        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
-
-        if (printerJob.printDialog ())
-        {
-          try
-          {
-            printerJob.setPrintable (formatBox.getSelectedReportMaker ());
-            printerJob.print ();
-          }
-          catch (PrinterException e)
-          {
-            e.printStackTrace ();
-          }
-        }
-      }
-    });
-  }
-
-  private void saveFile ()
-  {
-  }
-
-  private void closeWindow ()
-  {
-    windowSaver.saveWindow ();
-  }
+  //  private void printFile ()
+  //  {
+  //    SwingUtilities.invokeLater (new Runnable ()
+  //    {
+  //      @Override
+  //      public void run ()
+  //      {
+  //        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
+  //
+  //        if (printerJob.printDialog ())
+  //        {
+  //          try
+  //          {
+  //            printerJob.setPrintable (formatBox.getSelectedReportMaker ());
+  //            printerJob.print ();
+  //          }
+  //          catch (PrinterException e)
+  //          {
+  //            e.printStackTrace ();
+  //          }
+  //        }
+  //      }
+  //    });
+  //  }
+  //
+  //  private void saveFile ()
+  //  {
+  //  }
 
   @Override
   public void paginationChanged (Pagination pagination)
