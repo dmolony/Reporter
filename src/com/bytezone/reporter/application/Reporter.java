26f1999789b4f6be7fc392d3548bd5bb01e9684c
Denis Molony <dmolony@iinet.net.au>
2015-07-31 08:51:57 +1000

    ReporterScene is a widget

--- src/com/bytezone/reporter/application/Reporter.java
+++ src/com/bytezone/reporter/application/Reporter.java
@@ -8,17 +8,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Reporter extends Application
-//    implements PaginationChangeListener, FileSelectionListener
 {
-  //  private final static String OS = System.getProperty ("os.name");
-  //  private final static boolean MAC_MENUBAR = OS != null && OS.startsWith ("Mac");
-  //
-  //  private FormatBox formatBox;
-  //  private ReportData reportData;
-  //
   private final BorderPane borderPane = new BorderPane ();
-  //  private final MenuBar menuBar = new MenuBar ();
-  //
   private WindowSaver windowSaver;
   private final Preferences prefs = Preferences.userNodeForPackage (this.getClass ());
 
@@ -27,125 +18,17 @@ public class Reporter extends Application
   {
     String home = System.getProperty ("user.home") + "/Dropbox/testfiles";
 
-    //    Scene scene = getScene (home, this);
-    Scene scene = new ReporterScene (borderPane);
+    Scene scene = new ReporterScene (prefs, borderPane);
     primaryStage.setTitle ("Reporter");
     primaryStage.setScene (scene);
     primaryStage.setOnCloseRequest (e -> closeWindow ());
 
     windowSaver = new WindowSaver (prefs, primaryStage, "Reporter");
-    if (!windowSaver.restoreWindow ())
-      primaryStage.centerOnScreen ();
+    windowSaver.restoreWindow ();
 
     primaryStage.show ();
   }
 
-  //  public Scene getScene (String home, FileSelectionListener listener)
-  //  {
-  //    TreePanel treePanel = new TreePanel (prefs);
-  //    treePanel.addFileSelectionListener (listener);
-  //    StackPane stackPane = new StackPane ();
-  //    stackPane.setPrefWidth (180);
-  //
-  //    TreeView<FileNode> tree = treePanel.getTree (home);
-  //    stackPane.getChildren ().add (tree);
-  //
-  //    borderPane.setLeft (stackPane);
-  //    borderPane.setTop (menuBar);
-  //
-  //    menuBar.getMenus ().addAll (getFileMenu ());
-  //
-  //    if (MAC_MENUBAR)
-  //      menuBar.useSystemMenuBarProperty ().set (true);
-  //
-  //    Scene scene = new Scene (borderPane, 800, 592);
-  //    tree.requestFocus ();
-  //
-  //    return scene;
-  //  }
-
-  //  @Override
-  //  public void fileSelected (FileNode fileNode)
-  //  {
-  //    formatBox = fileNode.formatBox;
-  //    borderPane.setRight (formatBox.getFormattingBox ());
-  //    formatBox.setFileNode (fileNode, this);
-  //  }
-
-  //  private Menu getFileMenu ()
-  //  {
-  //    Menu menuFile = new Menu ("File");
-  //
-  //    MenuItem menuItemOpen = getMenuItem ("Open...", e -> openFile (), KeyCode.O);
-  //    MenuItem menuItemSave = getMenuItem ("Save...", e -> saveFile (), KeyCode.S);
-  //    MenuItem menuItemPrint = getMenuItem ("Page setup", e -> pageSetup (), null);
-  //    MenuItem menuItemPageSetup = getMenuItem ("Print", e -> printFile (), KeyCode.P);
-  //    MenuItem menuItemClose = getMenuItem ("Close window", e -> closeWindow (), KeyCode.W);
-  //
-  //    menuFile.getItems ().addAll (menuItemOpen, menuItemSave, menuItemPageSetup,
-  //                                 menuItemPrint, menuItemClose);
-  //    return menuFile;
-  //  }
-
-  //  private MenuItem getMenuItem (String text, EventHandler<ActionEvent> eventHandler,
-  //      KeyCode keyCode)
-  //  {
-  //    MenuItem menuItem = new MenuItem (text);
-  //    menuItem.setOnAction (eventHandler);
-  //    if (keyCode != null)
-  //      menuItem.setAccelerator (new KeyCodeCombination (keyCode,
-  //          KeyCombination.SHORTCUT_DOWN));
-  //    return menuItem;
-  //  }
-  //
-  //  private void openFile ()
-  //  {
-  //    System.out.println ("Open");
-  //  }
-  //
-  //  private void pageSetup ()
-  //  {
-  //    SwingUtilities.invokeLater (new Runnable ()
-  //    {
-  //      @Override
-  //      public void run ()
-  //      {
-  //        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
-  //        PageFormat pageFormat = printerJob.defaultPage ();
-  //        printerJob.pageDialog (pageFormat);
-  //      }
-  //    });
-  //  }
-  //
-  //  private void printFile ()
-  //  {
-  //    SwingUtilities.invokeLater (new Runnable ()
-  //    {
-  //      @Override
-  //      public void run ()
-  //      {
-  //        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
-  //
-  //        if (printerJob.printDialog ())
-  //        {
-  //          try
-  //          {
-  //            printerJob.setPrintable (formatBox.getSelectedReportMaker ());
-  //            printerJob.print ();
-  //          }
-  //          catch (PrinterException e)
-  //          {
-  //            e.printStackTrace ();
-  //          }
-  //        }
-  //      }
-  //    });
-  //  }
-  //
-  //  private void saveFile ()
-  //  {
-  //  }
-  //
   private void closeWindow ()
   {
     windowSaver.saveWindow ();
@@ -155,11 +38,4 @@ public class Reporter extends Application
   {
     launch (args);
   }
-
-  //  @Override
-  //  public void paginationChanged (Pagination pagination)
-  //  {
-  //    //    System.out.println (pagination);
-  //    borderPane.setCenter (pagination);
-  //  }
 }
--- src/com/bytezone/reporter/application/ReporterScene.java
+++ src/com/bytezone/reporter/application/ReporterScene.java
@@ -1,13 +1,25 @@
 package com.bytezone.reporter.application;
 
+import java.awt.print.PageFormat;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.util.prefs.Preferences;
+
+import javax.swing.SwingUtilities;
 
 import com.bytezone.reporter.application.TreePanel.FileNode;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
@@ -18,19 +30,19 @@ public class ReporterScene extends Scene
   private final static boolean MAC_MENUBAR = OS != null && OS.startsWith ("Mac");
 
   private FormatBox formatBox;
-  //  private ReportData reportData;
 
   private final BorderPane borderPane;
   private final MenuBar menuBar = new MenuBar ();
 
-  //  private WindowSaver windowSaver;
-  private final Preferences prefs = Preferences.userNodeForPackage (this.getClass ());
+  private final Preferences prefs;
 
-  public ReporterScene (BorderPane root)
+  public ReporterScene (Preferences prefs, BorderPane root)
   {
     super (root, 800, 592);
 
     this.borderPane = root;
+    this.prefs = prefs;
+
     String home = System.getProperty ("user.home") + "/Dropbox/testfiles";
 
     TreePanel treePanel = new TreePanel (prefs);
@@ -44,12 +56,11 @@ public class ReporterScene extends Scene
     borderPane.setLeft (stackPane);
     borderPane.setTop (menuBar);
 
-    //    menuBar.getMenus ().addAll (getFileMenu ());
+    menuBar.getMenus ().addAll (getFileMenu ());
 
-    //    if (MAC_MENUBAR)
-    //      menuBar.useSystemMenuBarProperty ().set (true);
+    if (MAC_MENUBAR)
+      menuBar.useSystemMenuBarProperty ().set (true);
 
-    //    Scene scene = new Scene (borderPane, 800, 592);
     tree.requestFocus ();
   }
 
@@ -61,84 +72,89 @@ public class ReporterScene extends Scene
     formatBox.setFileNode (fileNode, this);
   }
 
-  //  private Menu getFileMenu ()
-  //  {
-  //    Menu menuFile = new Menu ("File");
-  //
-  //    MenuItem menuItemOpen = getMenuItem ("Open...", e -> openFile (), KeyCode.O);
-  //    MenuItem menuItemSave = getMenuItem ("Save...", e -> saveFile (), KeyCode.S);
-  //    MenuItem menuItemPrint = getMenuItem ("Page setup", e -> pageSetup (), null);
-  //    MenuItem menuItemPageSetup = getMenuItem ("Print", e -> printFile (), KeyCode.P);
-  //    MenuItem menuItemClose = getMenuItem ("Close window", e -> closeWindow (), KeyCode.W);
-  //
-  //    menuFile.getItems ().addAll (menuItemOpen, menuItemSave, menuItemPageSetup,
-  //                                 menuItemPrint, menuItemClose);
-  //    return menuFile;
-  //  }
-  //
-  //  private MenuItem getMenuItem (String text, EventHandler<ActionEvent> eventHandler,
-  //      KeyCode keyCode)
-  //  {
-  //    MenuItem menuItem = new MenuItem (text);
-  //    menuItem.setOnAction (eventHandler);
-  //    if (keyCode != null)
-  //      menuItem.setAccelerator (new KeyCodeCombination (keyCode,
-  //          KeyCombination.SHORTCUT_DOWN));
-  //    return menuItem;
-  //  }
+  private Menu getFileMenu ()
+  {
+    Menu menuFile = new Menu ("File");
 
-  //  private void openFile ()
-  //  {
-  //    System.out.println ("Open");
-  //  }
+    MenuItem menuItemOpen = getMenuItem ("Open...", e -> openFile (), KeyCode.O);
+    MenuItem menuItemSave = getMenuItem ("Save...", e -> saveFile (), KeyCode.S);
+    MenuItem menuItemPrint = getMenuItem ("Page setup", e -> pageSetup (), null);
+    MenuItem menuItemPageSetup = getMenuItem ("Print", e -> printFile (), KeyCode.P);
+    MenuItem menuItemClose = getMenuItem ("Close window", e -> closeWindow (), KeyCode.W);
 
-  //  private void pageSetup ()
-  //  {
-  //    SwingUtilities.invokeLater (new Runnable ()
-  //    {
-  //      @Override
-  //      public void run ()
-  //      {
-  //        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
-  //        PageFormat pageFormat = printerJob.defaultPage ();
-  //        printerJob.pageDialog (pageFormat);
-  //      }
-  //    });
-  //  }
+    menuFile.getItems ().addAll (menuItemOpen, menuItemSave, menuItemPageSetup,
+                                 menuItemPrint, menuItemClose);
+    return menuFile;
+  }
 
-  //  private void printFile ()
-  //  {
-  //    SwingUtilities.invokeLater (new Runnable ()
-  //    {
-  //      @Override
-  //      public void run ()
-  //      {
-  //        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
-  //
-  //        if (printerJob.printDialog ())
-  //        {
-  //          try
-  //          {
-  //            printerJob.setPrintable (formatBox.getSelectedReportMaker ());
-  //            printerJob.print ();
-  //          }
-  //          catch (PrinterException e)
-  //          {
-  //            e.printStackTrace ();
-  //          }
-  //        }
-  //      }
-  //    });
-  //  }
-  //
-  //  private void saveFile ()
-  //  {
-  //  }
+  private MenuItem getMenuItem (String text, EventHandler<ActionEvent> eventHandler,
+      KeyCode keyCode)
+  {
+    MenuItem menuItem = new MenuItem (text);
+    menuItem.setOnAction (eventHandler);
+    if (keyCode != null)
+      menuItem.setAccelerator (new KeyCodeCombination (keyCode,
+          KeyCombination.SHORTCUT_DOWN));
+    return menuItem;
+  }
+
+  private void openFile ()
+  {
+    System.out.println ("Open");
+  }
+
+  private void pageSetup ()
+  {
+    SwingUtilities.invokeLater (new Runnable ()
+    {
+      @Override
+      public void run ()
+      {
+        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
+        PageFormat pageFormat = printerJob.defaultPage ();
+        printerJob.pageDialog (pageFormat);
+      }
+    });
+  }
+
+  private void printFile ()
+  {
+    SwingUtilities.invokeLater (new Runnable ()
+    {
+      @Override
+      public void run ()
+      {
+        PrinterJob printerJob = PrinterJob.getPrinterJob ();// AWT
+
+        if (printerJob.printDialog ())
+        {
+          try
+          {
+            printerJob.setPrintable (formatBox.getSelectedReportMaker ());
+            printerJob.print ();
+          }
+          catch (PrinterException e)
+          {
+            e.printStackTrace ();
+          }
+        }
+      }
+    });
+  }
+
+  private void saveFile ()
+  {
+    System.out.println ("Save");
+  }
+
+  private void closeWindow ()
+  {
+    System.out.println ("Close");
+  }
 
   @Override
   public void paginationChanged (Pagination pagination)
   {
-    //    System.out.println (pagination);
     borderPane.setCenter (pagination);
   }
 }
