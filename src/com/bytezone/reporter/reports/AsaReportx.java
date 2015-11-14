d9eaf4b5510f8c6e0199da0b89b624ed1f347332
Denis Molony <dmolony@iinet.net.au>
2015-11-14 15:26:20 +1100

    renamed

--- src/com/bytezone/reporter/reports/AsaReportx.java
+++ src/com/bytezone/reporter/reports/AsaReportx.java
@@ -14,9 +14,9 @@ import com.bytezone.reporter.text.TextMaker;
  * '1:C' - Skip to channel 1-12 (channel 1 is top of page)
  */
 
-public class AsaReport extends DefaultReportMaker
+public class AsaReportx extends DefaultReportMaker
 {
-  public AsaReport (boolean newLine, boolean split)
+  public AsaReportx (boolean newLine, boolean split)
   {
     super ("ASA", newLine, split);
     weight = 1.1;
